3a7b34a5c1f3e1981cad3564f920d2b0947fbcae
coolba73 <coolba73@outlook.com>
2018-05-12 14:01:36 +0900

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { MainComponent } from "./main/main.component";
 import { AppRouting } from "./app.routing";
 import { provideRoutes } from "@angular/router";
+import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [
-    AppComponent
-    ,MainComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
